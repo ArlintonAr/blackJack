abf464a6f5ae55e71f44b40701295a5aef459ad4
ArlintonAr <arlintonarturodelgadoflores@gmail.com>
2023-05-16 10:55:02 -0500

    Agregando mensaje de resultado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -31,7 +31,7 @@ const getCards = () => {
     console.log(card)
     return card
 }
-const cardHtml = (card) => {
+const cardHtmlPlayer = (card) => {
     const playerCards = document.querySelector('#playerCards')
     let img = document.createElement('img')
     img.classList.add('size-cart')
@@ -39,42 +39,83 @@ const cardHtml = (card) => {
     playerCards.appendChild(img)
 }
 
+const cardHtmlComputer = (card) => {
+    const computerCards = document.querySelector('#computerCards')
+    let img = document.createElement('img')
+    img.classList.add('size-cart')
+    img.src= `assets/cartas/${card}.png`
+    computerCards.appendChild(img)
+}
 
 const valueCard = (card) => {
     const valor = card.substring(0, card.length - 1) //extraer el valor de la carta pero obviando el ultimo digito
     return (isNaN(valor)) ?
         (valor === 'A') ? 11 : 10
         : valor * 1
-    
 }
+
+
+
+const reqquestCard = document.querySelector('#reqquestCard')
+const punctuacionCount=document.querySelectorAll('#punctuacionCount')
+const puntuacionCountPc=document.querySelectorAll('#puntuacionCountPc')
+const stop=document.querySelector('#stop')
+
+let punctuacionPlayer=0
+let puntuacionPc=0
+
 //TURNO PC
-const computerShift=(punctuacionMin)=>{
-    const 
+const computerShift=(punctuactionMin)=>{
+
+    do {
+        const card=getCards()
+        puntuacionPc=puntuacionPc+ valueCard(card)
+        puntuacionCountPc[0].textContent=puntuacionPc
+        cardHtmlComputer(card)
+        if(punctuactionMin>21){
+            break
+        }
+    } while ((puntuacionPc<punctuactionMin) && (punctuactionMin<=21));
+
+    setTimeout(() => {     
+        if(puntuacionPc===punctuactionMin){
+            alert("No ganó ninguno")
+        }else if(punctuactionMin>21){
+            alert("La computadora gana")
+        }else if(puntuacionPc>21){
+            alert("Ud ha ganado")
+        }else{
+            alert("gana computadora")
+        }
+    }, 100);
+    
+
 }
 
 //EVENTOS
-const reqquestCard = document.querySelector('#reqquestCard')
-const punctuacionCount=document.querySelectorAll('#punctuacionCount')
-let punctuacionPlayer=0;
-
 reqquestCard.addEventListener('click', () => {
     const card=getCards()
     punctuacionPlayer=punctuacionPlayer + valueCard(card)
     punctuacionCount[0].textContent=punctuacionPlayer
-    cardHtml(card)
+    cardHtmlPlayer(card)
     if(punctuacionPlayer>21){
         console.log('Haz perdido el juego')
         reqquestCard.disabled=true
+        computerShift(punctuacionPlayer)
     }else if(punctuacionPlayer===21){
         console.log("Haz ganado el juego")
         reqquestCard.disabled=true
+        computerShift(punctuacionPlayer)
     }
 
+    
 })
 
-const punctuation=(value)=>{
-    if(valor){}
-}
 
+stop.addEventListener('click',()=>{
+    reqquestCard.disabled=true
+    stop.disabled=true
+    computerShift(punctuacionPlayer)
+})
 
 createDeck()
